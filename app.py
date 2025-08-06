f61ea71ef82b440b9f6012fa2a4aae8ec464c3ac
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2025-08-06 11:22:20 +0530

    Add accuracy metric

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
 import streamlit as st
+import pandas as pd
+import joblib
 
 st.set_page_config(page_title="Dashboard")
 st.title("Breast Cancer Detection 🎀")
+
+lr = joblib.load('model.pkl')
+
+df = pd.read_csv("data.csv")
+df = df.drop(['Unnamed: 32', 'id'], axis=1)
+df.diagnosis = [1 if value == "M" else 0 for value in df.diagnosis]
+
+y = df['diagnosis'] # target variable
+X = df.drop(['diagnosis'], axis=1)
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.30, random_state=42)
+
+y_pred = lr.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+st.metric("Model Accuracy", "{:.2f}".format(accuracy), border=True)
